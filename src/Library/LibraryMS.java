512e1c310030e3e6272900dd31299da518b3b456
DhavalDhimar <dhaval.dhimar@gmail.com>
2023-01-31 19:48:08 +0530

    remove out & .idea

--- src/Library/LibraryMS.java
+++ src/Library/LibraryMS.java
@@ -1,61 +1,82 @@
 package Library;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 //search by name
 //search by author
 //issue book
 //return book
+
+
 public class LibraryMS {
 
-    public static void main(String[] args) {
-        Library l=new Library();
+    String uname;
+    String pass;
 
-        int i=0;
+
+    public static void main(String[] args) {
+
+        LibraryMS lms=new LibraryMS();
+        Library l=new Library();
+        User u=new User();
+
         Scanner sc=new Scanner(System.in);
 
-        while (i==0) {
-            System.out.println("See all books in library enter 1");
-            System.out.println("Search book by a Name or Author enter 2");
-            System.out.println("Return a book enter 3");
-            System.out.println("For exit enter 4");
-            int input = sc.nextInt();
-            switch (input){
-                case 1:
-                    l.display();
-                    System.out.println("Issue a book enter 1");
-                    System.out.println("For go back enter 2");
-                    input=sc.nextInt();
-                    switch (input){
-                        case 1:
-                            System.out.println("thanks");
-                            break;
-                        case 2:
-                            System.out.println("t");
-                            break;
-                    }
-                    break;
-                case 2:
-                    System.out.println("For search by Name enter 1");
-                    System.out.println("For search by Author enter 2");
-                    input=sc.nextInt();
-                    switch (input){
-                        case 1:
-                            System.out.println("book by name");
-                            break;
-                        case 2:
-                            System.out.println("book by author");
-                            break;
-                    }
-                break;
-                case 3:
-                    System.out.println("book returned");
-                break;
-                case 4:
-                    i=input;
-                    System.out.println("thanks");
-                break;
+        System.out.println("enter your user name and password");
+         lms.uname=sc.nextLine();
+         lms.pass=sc.nextLine();
+
+        int i=0;
+
+        if (u.check()) {
+
+            while (i == 0) {
+
+                System.out.println("See all books in library enter 1");
+                System.out.println("Search book by a Name or Author enter 2");
+                System.out.println("Return a book enter 3");
+                System.out.println("For exit enter 4");
+                int input = sc.nextInt();
+                switch (input) {
+                    case 1:
+                        l.display();
+                        System.out.println("Issue a book enter 1");
+                        System.out.println("For go back enter 2");
+                        input = sc.nextInt();
+                        switch (input) {
+                            case 1:
+                                l.issueBook();
+                                break;
+                            case 2:
+                                break;
+                        }
+                        break;
+                    case 2:
+                        System.out.println("For search by Name enter 1");
+                        System.out.println("For search by Author enter 2");
+                        input = sc.nextInt();
+                        switch (input) {
+                            case 1:
+                                l.search_by_book();
+                                break;
+                            case 2:
+                                l.search_by_author();
+                                break;
+                        }
+                        break;
+                    case 3:
+                        System.out.println("book returned");
+                        break;
+                    case 4:
+                        i = input;
+                        System.out.println("thanks");
+                        break;
+                }
             }
+        }
+        else {
+            System.out.println("unauotherized");
         }
 
     }
--- src/Library/User.java
+++ src/Library/User.java
@@ -1,4 +1,47 @@
 package Library;
 
-public class Usre {
+import java.util.ArrayList;
+
+public class User {
+    String u_name,password;
+    ArrayList<User> users=new ArrayList();
+
+    User(String u_name,String password){
+        this.u_name=u_name;
+        this.password=password;
+    }
+
+    User (){
+        User u1=new User("dhaval","dhaval123");
+        users.add(u1);
+        User u2=new User("dhaval1","dhaval1123");
+        users.add(u2);
+        User u3=new User("dhaval2","dhaval2123");
+        users.add(u3);
+        User u4=new User("dhaval3","dhaval3123");
+        users.add(u4);
+        User u5=new User("dhaval4","dhaval4123");
+        users.add(u5);
+    }
+
+    @Override
+    public String toString() {
+        return
+                u_name +" "+ password ;
+    }
+
+    LibraryMS lms1=new LibraryMS();
+
+    public boolean check() {
+        for (User user : users) {
+            String u=u_name;
+            String p=password;
+            if(u==lms1.uname & p==password ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
